d6a0d32f05092cdf17cd07d100e954e948e9ec7f
umida-dev <umidaxonsodiqova10@gmail.com>
2025-09-16 23:34:50 +0500

    Bugungi o‘zgarishlar yoki izoh

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
